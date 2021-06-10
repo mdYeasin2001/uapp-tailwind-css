6dd913bab00ba0aac8bd728f77fd5201bfd1c746
Yeasin <yeasin30112001@gmail.com>
2021-06-09 19:58:22 -0600

    testing auto deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>UAPP Coming Soon</h1>
+      <h2>Testing auto deployment</h2>
     </div>
   );
 }
