d2813e5231c00826e211391afcd48ace362c6505
Yeasin <yeasin30112001@gmail.com>
2021-06-10 23:41:12 -0600

    header area done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Home from './components/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>UAPP Coming Soon</h1>
-      <h2>Testing auto deployment</h2>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import phone from '../../../images/phone.png';
+
+const Header = () => {
+    
+    return (
+        <div className="header-area">
+            <div className="container mx-auto px-10">
+                <div className="flex flex-col md:flex-row py-10 content-center">
+                    <div className="flex-1 self-center text-white">
+                        <h1 className="text-5xl font-bold mb-6">APP LAND <br /> PROMOTE YOUR APP</h1>
+                        <p className="text-xl mb-3">The Best Way To Promote Your App And Make A lot Of Selling, In HinataYou Will be Able To Use An Awesome, Clean And Responsive Design To Market Your App</p>
+                        <button className="uapp-btn-primary px-8 py-2 mr-2 rounded-3xl font-medium tracking-wide">DOWNLOAD</button>
+                        <button className="uapp-btn-primary px-8 py-2 ml-2 rounded-3xl font-medium tracking-wide">CONTACT US</button>
+                    </div>
+                    <div className="flex-1 self-center lg:ml-20">
+                        <img style={{ maxHeight: '590px' }} src={phone} alt="app" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
